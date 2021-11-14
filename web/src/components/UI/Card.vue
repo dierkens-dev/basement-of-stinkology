ffe7d4eb2d2786e3823517917f3c05b047e8f89d
Jacob Dierkens <westernreserve@gmail.com>
2021-11-13 20:12:33 -0500

    frame out card component

---
+++ web/src/components/UI/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Avatar</p>
+    <p>{{ gamertag }}</p>
+    <p>{{ name }}</p>
+    <p>{{ description }}</p>
+    <app-button class="block mt-5 p-3">Button</app-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import AppButton from './AppButton.vue';
+
+export default defineComponent({
+  name: 'TheCard',
+  components: { AppButton },
+  setup() {
+    const name: String = 'Name';
+    const description: String = 'Description';
+    const gamertag: String = 'Gamertag';
+    return { name, description, gamertag };
+  },
+});
+</script>
--- web/src/pages/index.vue
+++ web/src/pages/index.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="md:block md:font-light md:text-6xl p-2 m-2">
-        The Basement of Stinkology
-      </h1>
-    </div>
+    <TheCard />
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import TheCard from '@/components/UI/Card.vue';
+
+export default defineComponent({
+  components: { TheCard },
+  setup() {},
+});
+</script>
+
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
 .container {
   margin: 0 auto;
   min-height: 100vh;
