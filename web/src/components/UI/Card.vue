64be4192851354d9f1629dc20f3376f50d05f4ae
Jacob Dierkens <westernreserve@gmail.com>
2021-11-14 10:48:23 -0500

    update card layout

--- web/src/components/UI/Card.vue
+++ web/src/components/UI/Card.vue
@@ -1,23 +1,25 @@
 <template>
-  <div>
-    <p>Avatar</p>
-    <p>{{ gamertag }}</p>
-    <p>{{ name }}</p>
-    <p>{{ description }}</p>
-    <app-button class="block mt-5 p-3">Button</app-button>
+  <div class="border-2 border-black rounded-lg grid grid-cols-2 gap-4 w-96 m-3">
+    <div class="text-sm align-text-bottom mt-2">{{ name }}</div>
+    <div class="row-span-3 rounded-full bg-blue-500 h-40 w-40 m-2"></div>
+    <div class="text-3xl">{{ gamertag }}</div>
+    <div class="">{{ description }}</div>
+    <AppButton class="m-2.5 w-24 border-black text-lg border-4"
+      >Profile</AppButton
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
-import AppButton from './AppButton.vue';
+import AppButton from '@/components/UI/AppButton.vue';
 
 export default defineComponent({
   name: 'TheCard',
   components: { AppButton },
   setup() {
-    const name: String = 'Name';
-    const description: String = 'Description';
+    const name: String = 'First Lastname';
+    const description: String = 'Lorem ipsum dolor sit amet...';
     const gamertag: String = 'Gamertag';
     return { name, description, gamertag };
   },
