fa5dfbbeb11a40ee44152a9f71f0e620a7a7af64
Christopher Dierkens <cjdierkens@gmail.com>
2023-02-02 08:23:25 -0500

    feat: add movie views

--- app/routes/movies/$id.tsx
+++ app/routes/movies/$id.tsx
@@ -14,15 +14,44 @@ export async function loader({ params }: LoaderArgs) {
   }
 
   const movieDbData = await MovieDbClient.movieInfo(movie.themoviedbId);
+  const movieViews = await prisma.movieView.findMany({
+    where: {
+      movieId: movie.id,
+    },
+  });
 
   return json({
     movieDbData,
     movie,
+    movieViews,
   });
 }
 
 export default function MovieIdRoute() {
-  const data = useLoaderData<typeof loader>();
+  const {
+    movie,
+    movieDbData: { poster_path, title, tagline },
+    movieViews,
+  } = useLoaderData<typeof loader>();
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <div>
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src={`https://image.tmdb.org/t/p/w185/${poster_path}`}
+            alt="Movie poster"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">{title}</h1>
+            <p className="py-6">{tagline}</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <pre>{JSON.stringify(movieViews, null, 2)}</pre>
+      </div>
+    </div>
+  );
 }
