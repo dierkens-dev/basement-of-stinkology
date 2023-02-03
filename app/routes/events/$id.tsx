f8d4e3c35042f2b2ad06e5f33ff0b78e1dbe32d2
Christopher Dierkens <cjdierkens@gmail.com>
2023-02-03 09:43:17 -0500

    feat: show title and date on event page

--- app/routes/events/$id.tsx
+++ app/routes/events/$id.tsx
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { format } from "date-fns";
 import { MovieDbClient } from "~/services/moviedb.server";
 import { prisma } from "~/services/prisma.server";
 import { invariant } from "~/utils/invariant";
@@ -8,19 +9,29 @@ import { invariant } from "~/utils/invariant";
 export async function loader({ params }: LoaderArgs) {
   const id = params.id;
 
-  const movieViews = await prisma.movieView.findMany({
-    where: { eventId: id },
-    orderBy: { viewDateTime: "asc" },
+  const event = await prisma.event.findFirst({
+    where: { id },
     include: {
-      movie: true,
+      MovieViews: {
+        orderBy: { viewDateTime: "asc" },
+        include: {
+          movie: true,
+        },
+      },
     },
   });
 
+  if (!event) {
+    throw new Response(null, { status: 404, statusText: "Not Found" });
+  }
+
   const moviesData = await Promise.all(
-    movieViews.map(({ movie }) => MovieDbClient.movieInfo(movie.themoviedbId))
+    event.MovieViews.map(({ movie }) =>
+      MovieDbClient.movieInfo(movie.themoviedbId)
+    )
   );
 
-  const response = movieViews.map((movieView) => {
+  const movies = event.MovieViews.map((movieView) => {
     const movieDbData = moviesData.find(
       ({ id }) => id === movieView.movie.themoviedbId
     );
@@ -33,33 +44,45 @@ export async function loader({ params }: LoaderArgs) {
     };
   });
 
-  return json(response);
+  return json({ movies, event });
 }
 
 export default function MoviesIndexRoute() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex gap-4">
-      {data &&
-        data.map(({ movieView: { movie }, movieDbData: { poster_path } }) => {
-          return (
-            <div
-              key={movie.id}
-              className="card w-38 bg-base-100 shadow-xl hover:scale-110 transition-transform"
-            >
-              <figure>
-                <Link to={`/movies/${movie.id}`}>
-                  <img
-                    className="object-contain"
-                    src={`https://www.themoviedb.org/t/p/w154/${poster_path}`}
-                    alt="Movie"
-                  />
-                </Link>
-              </figure>
-            </div>
-          );
-        })}
-    </div>
+    <>
+      <h1 className="text-3xl font-bold">{data.event.name}</h1>
+
+      {data.event.date ? (
+        <p>{format(new Date(data.event.date), "PP")}</p>
+      ) : null}
+
+      <h2 className="text-2xl font-bold mt-3 mb-1">Movies</h2>
+
+      <div className="flex gap-4">
+        {data &&
+          data.movies.map(
+            ({ movieView: { movie }, movieDbData: { poster_path } }) => {
+              return (
+                <div
+                  key={movie.id}
+                  className="card w-38 bg-base-100 shadow-xl hover:scale-110 transition-transform"
+                >
+                  <figure>
+                    <Link to={`/movies/${movie.id}`}>
+                      <img
+                        className="object-contain"
+                        src={`https://www.themoviedb.org/t/p/w154/${poster_path}`}
+                        alt="Movie"
+                      />
+                    </Link>
+                  </figure>
+                </div>
+              );
+            }
+          )}
+      </div>
+    </>
   );
 }
