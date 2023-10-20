7039f58b77f1f1aa098e80c1b1c235ae4cedfee6
Christopher Dierkens <cjdierkens@gmail.com>
2023-10-20 00:03:35 -0400

    fix: prettier ignore precedin space removal

--- src/features/movies/lib/moviedb/index.ts
+++ src/features/movies/lib/moviedb/index.ts
@@ -2,12 +2,12 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export { MovieDbClient } from "./MovieDbClient";
+export { MovieDbClient } from './MovieDbClient';
 
-export { ApiError } from "./core/ApiError";
-export { BaseHttpRequest } from "./core/BaseHttpRequest";
-export { CancelError, CancelablePromise } from "./core/CancelablePromise";
-export { OpenAPI } from "./core/OpenAPI";
-export type { OpenAPIConfig } from "./core/OpenAPI";
+export { ApiError } from './core/ApiError';
+export { BaseHttpRequest } from './core/BaseHttpRequest';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
 
-export { DefaultService } from "./services/DefaultService";
+export { DefaultService } from './services/DefaultService';
