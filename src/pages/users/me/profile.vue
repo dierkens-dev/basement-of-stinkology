58c3c65d48a1f4c09483d122a2d46186d17d75e2
Christopher Dierkens <cjdierkens@gmail.com>
2023-11-02 21:43:07 -0400

    wip: profile

--- src/pages/users/me/profile.vue
+++ src/pages/users/me/profile.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 const { data } = useAuth();
 const user = data.value?.user;
+
+invariant(user, "Something went wrong");
 </script>
 
 <template>
-  <div class="prose m-auto">
-    <h1>Profile</h1>
-    <pre>{{ JSON.stringify(user, null, 2) }}</pre>
+  <div class="flex h-full justify-center items-center">
+    <div class="card w-96 bg-base-100 border-base-200 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img
+          v-if="user.avatar"
+          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
