80ff3ea9c742eaae987db8a9f873ca02dd8f1883
Christopher Dierkens <cjdierkens@gmail.com>
2023-11-19 16:11:04 -0500

    fix: call the functions

--- infrastructure/app/network/bos.network.ts
+++ infrastructure/app/network/bos.network.ts
@@ -2,7 +2,7 @@ import * as gcp from "@pulumi/gcp";
 import * as pulumi from "@pulumi/pulumi";
 
 export const bosNetwork = new gcp.compute.Network("bos-network", {
-  name: pulumi.interpolate`bos-network-${pulumi.getStack}`,
+  name: pulumi.interpolate`bos-network-${pulumi.getStack()}`,
 });
 
 new gcp.compute.Firewall("bos-firewall", {
