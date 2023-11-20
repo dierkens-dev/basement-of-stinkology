d45b1c90699e4bb2c401499d7a43fd8a546de8d5
Christopher Dierkens <cjdierkens@gmail.com>
2023-11-19 22:12:57 -0500

    chore: it really is a chore at this point

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -3,9 +3,8 @@ import smtp from "nodemailer-smtp-transport";
 
 export const transport = mailer.createTransport(
   smtp({
-    host: "in-v3.mailjet.com",
+    host: "in.mailjet.com",
     port: 2525,
-    secure: true,
     auth: {
       user: process.env.BOS_MAILJET_API_KEY,
       pass: process.env.BOS_MAILJET_API_SECRET,
