973461bede358eeac7ca3bfe408a31ce16d778d2
Christopher Dierkens <cjdierkens@gmail.com>
2023-11-19 21:47:12 -0500

    chore: insecure

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -5,7 +5,6 @@ export const transport = mailer.createTransport(
   smtp({
     host: "in-v3.mailjet.com",
     port: 2525,
-    secure: true,
     auth: {
       user: process.env.BOS_MAILJET_API_KEY,
       pass: process.env.BOS_MAILJET_API_SECRET,
