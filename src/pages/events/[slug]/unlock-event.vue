5c620ae824f87198fa66aba7fd2cf5c544c08de0
Jacob Dierkens <westernreserve@gmail.com>
2025-02-09 17:12:23 -0500

    show lock icon on locked events, allow admin to unlock events

---
+++ src/pages/events/[slug]/unlock-event.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import { escapeDialog } from "~/utils/escapeDialog";
+
+const { params } = useRoute();
+const slug = params.slug;
+
+const { data: event } = await useFetch(`/api/events/${slug}`);
+
+const isSubmitting = ref(false);
+const handleConfirm = async () => {
+  isSubmitting.value = true;
+  await $fetch(`/api/events/${slug}/unlock`, {
+    method: "POST",
+  });
+
+  await navigateTo({
+    path: `/events/${slug}`,
+  });
+
+  await refreshNuxtData(`events/${slug}`);
+
+  isSubmitting.value = false;
+};
+const { path } = useRoute();
+</script>
+
+<template>
+  <dialog
+    class="modal modal-open modal-top sm:modal-middle"
+    open
+    @keydown.esc="escapeDialog(path)"
+  >
+    <div v-if="event?.data" class="modal-box prose">
+      <h2>Unlock Event</h2>
+
+      <p>
+        You are about to unlock the event
+        <strong>{{ event.data.name }}.</strong>
+      </p>
+      <div class="modal-action">
+        <NuxtLink :to="`/events/${event.data.slug}`" class="btn btn-secondary"
+          >Cancel</NuxtLink
+        >
+        <SubmitButton
+          ref="confirm"
+          :is-loading="isSubmitting"
+          :disabled="isSubmitting"
+          @click="handleConfirm"
+        >
+          Confirm
+        </SubmitButton>
+      </div>
+    </div>
+    <div v-else class="loading loading-bars loading-lg"></div>
+
+    <div class="modal-backdrop bg-black bg-opacity-30">
+      <NuxtLink :to="`/events/${slug}`">Close Modal</NuxtLink>
+    </div>
+  </dialog>
+</template>
